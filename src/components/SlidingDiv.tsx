b3e7a7c87844c75ac4b8050c96d82b7a2f31caf4
Saikat Kundu <kundusaikat32@gmail.com>
2023-05-19 01:55:07 +0530

    Fix: Welcome in homepage

--- src/components/SlidingDiv.tsx
+++ src/components/SlidingDiv.tsx
@@ -1,4 +1,4 @@
-import {  motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 export const SlidingDiv = () => {
   const motionDivs = [
@@ -28,9 +28,9 @@ export const SlidingDiv = () => {
     },
   ];
   return (
-    <div className="flex items-center justify-center h-[92vh] ">
-      <div className="grid grid-cols-2 ">
-      
+    <div className="relative">
+      <div className="flex items-center justify-center h-[92vh] absolute w-full">
+        <div className="grid grid-cols-2 ">
           {motionDivs.map((motionDiv, index) => (
             <motion.div
               key={index}
@@ -46,6 +46,23 @@ export const SlidingDiv = () => {
               className={`w-[10vw] h-[10vw] ${motionDiv.className}`}
             ></motion.div>
           ))}
+        </div>
+      </div>
+      <div className="flex items-center justify-center h-[92vh] absolute w-full">
+        <motion.div
+          initial={{ opacity: "100%" }}
+          animate={{ opacity: "0%" }}
+          exit={{ opacity: "100%" }}
+          transition={{
+            duration: 3,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+          className="w-[15vw] h-[15vw] flex items-center justify-center border-2 border-black rounded-full"
+        >
+          <p className="text-2xl font-bold font-[cursive]">Welcome</p>
+        </motion.div>
       </div>
     </div>
   );
